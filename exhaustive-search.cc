2df79ffefc8faddb7c192cae9e00d6e5a8025673
Tiziano Mueller <dev-zero@gentoo.org>
2011-01-25 11:39:39 +0100

    Factored storage container and common routines on it out to a separate class.

--- exhaustive-search.cc
+++ exhaustive-search.cc
@@ -32,19 +32,75 @@ namespace ublas
     using namespace boost::numeric::ublas;
 }
 
-void target_appender(std::shared_ptr<std::vector<Target>> targets,
+class MovingTargetTSP
+{
+private:
+    std::shared_ptr<std::vector<Target>> _targets;
+public:
+    MovingTargetTSP()
+    {
+        _targets.reset(new std::vector<Target>);
+    }
+    void add_target(const double& x, const double& y, const double& z,
+        const double& v_x, const double& v_y, const double& v_z,
+        const std::string& name)
+    {
+        Target t(x, y, z, v_x, v_y, v_z, name);
+        _targets->push_back(t);
+    }
+
+    /* calculate the cartesian product of the target set and execute the
+     * specified function on all pairs
+     * TODO: remove the Target struct and use (array, string, array, string) for the func signature
+     */
+    typedef std::function<void (const Target& a, const Target& b)> cartesian_product_func;
+    void for_each_pair(cartesian_product_func func)
+    {
+        for (std::vector<Target>::const_iterator i(_targets->begin()), i_end(_targets->end()); i != i_end; ++i)
+        {
+            for (std::vector<Target>::const_iterator j(_targets->begin()); j != i_end; ++j)
+            {
+                func(*i, *j);
+            }
+        }
+    }
+
+    void for_each_target(std::function<void (const Target&)> func)
+    {
+        for (std::vector<Target>::const_iterator i(_targets->begin()), i_end(_targets->end()); i != i_end; ++i)
+            func(*i);
+    }
+
+    void for_each_permutated_target(std::function<void (const Target&, size_t)> func, std::function<size_t (size_t)> permutation)
+    {
+        for (size_t n(0); n < _targets->size(); ++n)
+            func(_targets->at(permutation(n)), n);
+    }
+
+
+    Target get_target(size_t n) const
+    {
+        return _targets->at(n);
+    }
+
+    size_t size() const
+    {
+        return _targets->size();
+    }
+};
+
+/* output our targets while they're added */
+void target_appender(MovingTargetTSP& app,
         const double& x, const double& y, const double& z,
         const double& v_x, const double& v_y, const double& v_z,
         const std::string& name)
 {
-    Target t(x, y, z, v_x, v_y, v_z, name);
-    targets->push_back(t);
+    app.add_target(x, y, z, v_x, v_y, v_z, name);
     std::cout << "target added" << std::endl;
-    std::cout << "    name:     " << t.name << std::endl;
-    std::cout << "    position: " << t.position << std::endl;
-    std::cout << "    velocity: " << t.velocity << std::endl;
+    std::cout << "    name:     " << name << std::endl;
+    std::cout << "    position: " << x << ", " << y << ", " << z << std::endl;
+    std::cout << "    velocity: " << v_x << ", " << v_y << ", " << v_z << std::endl;
 }
-
 
 /* calculate the time duration to go from s_position to e_position which moves with e_velocity when travelling with velocity v */
 inline double calculate_time(const double& v, const ublas::vector<double>& s_position, const ublas::vector<double>& e_position, const ublas::vector<double>& e_velocity)
@@ -70,6 +126,19 @@ inline double calculate_time(const double& v, const ublas::vector<double>& s_pos
     else
         return std::min(t1, t2);
 }
+
+/* calculate the distance between two targets
+ * and update the shortest_distance if the distance is shorter than shortest_distance */
+void calculate_distance_and_direct_travelling_time(double* shortest_distance, double v, const Target& a, const Target& b)
+{
+    double distance(ublas::norm_1(a.position - b.position));
+    std::cout << "current distance between targets " << a.name << " and " << b.name << " is: " << distance << std::endl;
+    std::cout << "    direct travelling time: " << calculate_time(v, a.position, b.position, b.velocity) << std::endl;
+
+    if ( (distance > 0.0) && (distance < *shortest_distance) ) // ignore the distance if it is 0 (to make sure we have a positiv radius)
+        *shortest_distance = distance;
+}
+
 
 /* creates the required structures to draw a target using OSG */
 inline osg::ref_ptr<osg::Geode> create_target(const osg::Vec3f& position, const double& radius, const osg::Vec4& color = osg::Vec4(1, 1, 0.5, 0.1))
@@ -111,6 +180,7 @@ int main(int argc, char* argv[])
     double v(5.0);
     std::string filename("");
 
+    /* handle commandline arguments */
     try
     {
         int end;
@@ -141,43 +211,33 @@ int main(int argc, char* argv[])
 
     /* load the targets from the file */
     FileSource<CSVParser> file(filename.c_str()); // create a file source using a CSVParser
-    std::shared_ptr<std::vector<Target>> targets(new std::vector<Target>); // container holding our targets
-    file.load( std::bind(&target_appender, targets,
+
+    MovingTargetTSP app;
+
+    target_appender(app, 0, 0, 0, 0, 0, 0, "start"); // create a fake start-target (currently hardcoded at 0/0/0 with velocity 0/0/0)
+
+    file.load( std::bind(target_appender, app,
                 std::placeholders::_1, std::placeholders::_2, std::placeholders::_3,
                 std::placeholders::_4, std::placeholders::_5, std::placeholders::_6,
-                std::placeholders::_7) ); // bind the target-container-pointer to our appender-function and passing that to the source
+                std::placeholders::_7) ); // bind the MovingTargetTSP instance to our appender-function and passing that to the source
 
-    Target start(0, 0, 0, 0, 0, 0, "start"); // create a fake start-target (currently hardcoded at 0/0/0 with velocity 0/0/0)
     const double start_time(0.0); // initialize our start-time (could be hardocded but we may want to start at a different time than "now")
 
-    // insert the start at front
-    targets->insert(targets->begin(), start);
-
     double smallest_start_distance(std::numeric_limits<double>::infinity()); // the smallest distance between two targets at t=0, mostly needed to calculated an appropriate radius for the 3D-objects
+    
+    app.for_each_pair( std::bind(
+                calculate_distance_and_direct_travelling_time,
+                &smallest_start_distance, v,
+                std::placeholders::_1, std::placeholders::_2) );
 
-    /* calculate the distance at t=0 between all targets */
-    for (std::vector<Target>::const_iterator i(targets->begin()), i_end(targets->end()); i != i_end; ++i)
-    {
-        for (std::vector<Target>::const_iterator j(i+1); j != i_end; ++j)
-        {
-            double distance(ublas::norm_1(j->position - i->position));
-            std::cout << "distance between targets " << i->name << " and " << j->name << " is: " << distance << std::endl;
-            std::cout << "    direct travelling time: " << calculate_time(v, i->position, j->position, j->velocity) << std::endl;
-
-            if ( (distance > 0.0) && (distance < smallest_start_distance) ) // ignore the distance if it is 0 (to make sure we have a positiv radius)
-                smallest_start_distance = distance;
-        }
-    }
-
-    // ... and at the end such that we get a closed cycle (doing it here to simplify the algorithm aboe)
-    targets->push_back(start);
+    target_appender(app, 0, 0, 0, 0, 0, 0, "end");
 
     /*
      * calculate the travelling time for all permutations
      */
 
-    std::vector<unsigned long> shortest_path_permutation; // holding the permutation with the shortest path, not preallocating by intention
-    std::vector<double> shortest_path_times(targets->size()), path_times(targets->size()); // stores the corresponding times (answer to: at which 't' did we hit the target?)
+    std::vector<unsigned long> shortest_path_permutation; // holding the permutation with the shortest path
+    std::vector<double> shortest_path_times(app.size()), path_times(app.size()); // stores the corresponding times (answer to: at which 't' did we hit the target?)
     double shortest_path_total_time(std::numeric_limits<double>::infinity()); // ... and how long was the tour
 
     std::cout << std::endl;
@@ -185,7 +245,7 @@ int main(int argc, char* argv[])
     std::cout << "permutations" << std::endl;
     std::cout << "============" << std::endl << std::endl;
 
-    std::vector<unsigned long> permutation(targets->size());
+    std::vector<unsigned long> permutation(app.size());
     unsigned long count(0);
     std::generate(permutation.begin(), permutation.end(), [&count]() { return count++; });
 
@@ -193,16 +253,16 @@ int main(int argc, char* argv[])
     {
         double total_t(start_time);
 
-        std::cout << targets->at(permutation.front()).name << " (" << total_t << ")";
+        std::cout << app.get_target(permutation.front()).name << " (" << total_t << ")";
         path_times.front() = total_t;
 
         for (auto j(permutation.begin()), j_end(permutation.end()-1); j != j_end; ++j)
         {
             total_t += calculate_time(v,
-                    targets->at(*j).position + total_t*targets->at(*j).velocity,
-                    targets->at(*(j+1)).position + total_t*targets->at(*(j+1)).velocity,
-                    targets->at(*(j+1)).velocity);
-            std::cout << " -> " << targets->at(*(j+1)).name << " (" << total_t << ")";
+                    app.get_target(*j).position + total_t*app.get_target(*j).velocity,
+                    app.get_target(*(j+1)).position + total_t*app.get_target(*(j+1)).velocity,
+                    app.get_target(*(j+1)).velocity);
+            std::cout << " -> " << app.get_target(*(j+1)).name << " (" << total_t << ")";
             path_times.at(std::distance(permutation.begin(), j+1)) = total_t;
         }
         std::cout << " = " << total_t << std::endl;
@@ -226,10 +286,10 @@ int main(int argc, char* argv[])
     /*
      * repeat the shortest path
      */
-    std::cout << "shortest path was: " << targets->at(shortest_path_permutation.front()).name << " (" << shortest_path_times.front() << ")";
+    std::cout << "shortest path was: " << app.get_target(shortest_path_permutation.front()).name << " (" << shortest_path_times.front() << ")";
     for (auto i(shortest_path_permutation.begin()), i_end(shortest_path_permutation.end()-1), i_begin(shortest_path_permutation.begin()); i != i_end; ++i)
     {
-        std::cout << " -> " << targets->at(*(i+1)).name << " (" << shortest_path_times.at(std::distance(i_begin, i)+1) << ")";
+        std::cout << " -> " << app.get_target(*(i+1)).name << " (" << shortest_path_times.at(std::distance(i_begin, i)+1) << ")";
     }
     std::cout << std::endl;
 
@@ -243,26 +303,27 @@ int main(int argc, char* argv[])
             std::bind(addStarToRoot, root.get(), std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5));
 
     osg::Vec4 color(1.0f, 0.0f, 0.0f, 1.0f); // mark the start target (red)
-    for (auto i(targets->begin()), i_end(targets->end()-1); i != i_end; ++i)
-    {
-        addStar(i->position(0), i->position(1), i->position(2), smallest_start_distance/2.0, color);
-        color[0] = 0.0f; color[2] = 1.0f; // the targets should be blue
-    }
-
+    app.for_each_target(
+            [&color, &smallest_start_distance, &addStar](const Target& t) {
+                addStar(t.position(0), t.position(1), t.position(2), smallest_start_distance/2.0, color);
+                color[0] = 0.0f; color[2] = 1.0f; // the targets should be blue
+                } );
 
     osg::ref_ptr<osg::Vec3Array> target_positions_at_hit(new osg::Vec3Array); // store the positions to draw the tour
 
-    for (auto i(shortest_path_permutation.begin()), i_end(shortest_path_permutation.end()-1), i_begin(shortest_path_permutation.begin()); i != i_end; ++i)
-    {
-        osg::Vec3 position(
-                targets->at(*i).position(0) + shortest_path_times.at(std::distance(i_begin, i))*targets->at(*i).velocity(0),
-                targets->at(*i).position(1) + shortest_path_times.at(std::distance(i_begin, i))*targets->at(*i).velocity(1),
-                targets->at(*i).position(2) + shortest_path_times.at(std::distance(i_begin, i))*targets->at(*i).velocity(2));
-
-        addStar(position[0], position[1], position[2], smallest_start_distance/2.0, color);
-
-        target_positions_at_hit->push_back(position);
-    }
+    app.for_each_permutated_target(
+            [&shortest_path_times, &target_positions_at_hit, &addStar, &color, smallest_start_distance](const Target& t, size_t n) {
+                osg::Vec3 position(
+                    t.position(0) + shortest_path_times.at(n)*t.velocity(0),
+                    t.position(1) + shortest_path_times.at(n)*t.velocity(1),
+                    t.position(2) + shortest_path_times.at(n)*t.velocity(2));
+                addStar(position[0], position[1], position[2], smallest_start_distance/2.0, color);
+                target_positions_at_hit->push_back(position);
+                },
+            [&shortest_path_permutation](size_t n) {
+                return shortest_path_permutation[n];
+            }
+        );
 
     // draw the tour
     root->addChild(create_tour(target_positions_at_hit).get());
