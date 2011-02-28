3a30a0683aab52ed58b3750b18d0d82e41d980c8
Tiziano Mueller <dev-zero@gentoo.org>
2011-02-28 18:50:57 +0100

    Get more data from the file and added descriptions for the fields.

--- parser/hip2.cc
+++ parser/hip2.cc
@@ -74,15 +74,22 @@ Parser::operator()(const std::string& text, appender_tuple_func func)
             throw InvalidLine();
 
         if (!func(std::make_tuple(
-                atoi(text.substr(offset, 6).c_str()),
-                atoi(text.substr(offset + 7, 3).c_str()),
-                atof(text.substr(offset + 15, 13).c_str()),
-                atof(text.substr(offset + 29, 13).c_str()),
-                atof(text.substr(offset + 43, 7).c_str()),
-                atof(text.substr(offset + 83, 6).c_str()),
-                atof(text.substr(offset + 51, 8).c_str()),
-                atof(text.substr(offset + 60, 8).c_str()),
-                atof(text.substr(offset + 152, 6).c_str()))))
+                atoi(text.substr(offset +   0,  6).c_str()),
+                atoi(text.substr(offset +   7,  3).c_str()),
+                atoi(text.substr(offset +   8,  1).c_str()),
+                atoi(text.substr(offset +   8,  1).c_str()),
+                atof(text.substr(offset +  15, 13).c_str()),
+                atof(text.substr(offset +  29, 13).c_str()),
+                atof(text.substr(offset +  43,  7).c_str()),
+                atof(text.substr(offset +  51,  8).c_str()),
+                atof(text.substr(offset +  60,  8).c_str()),
+                atof(text.substr(offset +  69,  6).c_str()),
+                atof(text.substr(offset +  76,  6).c_str()),
+                atof(text.substr(offset +  83,  6).c_str()),
+                atof(text.substr(offset + 152,  6).c_str()),
+                atof(text.substr(offset + 159,  5).c_str()),
+                atof(text.substr(offset + 165,  6).c_str())
+                )))
             return;
 
         // we found the last line without carriage return
--- parser/hip2.hh
+++ parser/hip2.hh
@@ -34,8 +34,23 @@ struct Parser
     void operator()(const std::string& text, appender_func func);
 
     /* spec:
-     * same as for the functor above */
-    typedef std::tuple<int, int, double, double, double, double, double, double, double> data;
+     * HIP identifier,
+     * solution type new reduction,
+     * solution type old reduction,
+     * number of components
+     * right ascension,
+     * declination,
+     * parallax,
+     * proper motion in right ascension,
+     * proper motion in declination,
+     * formal error on RArad
+     * formal error on DErad
+     * formal error on parallax,
+     * colour index,
+     * formal error on colour index,
+     * V-I colour index,
+     */
+    typedef std::tuple<int, int, short, short, double, double, double, double, double, double, double, double, double, double, double> data;
     typedef std::function<bool (const data&)> appender_tuple_func;
     void operator()(const std::string& text, appender_tuple_func func);
 };
@@ -44,13 +59,37 @@ enum fields
 {
     ID = 0,
     SOLUTIONTYPE,
+    SOLUTIONTYPE_OLD_REDUX,
+    NUMBER_OF_COMPONENTS,
     RIGHT_ASCENSION,
     DECLINATION,
     PARALLAX,
-    PARALLAX_ERR,
     PROPER_MOTION_IN_RA,
     PROPER_MOTION_IN_DECL,
-    COLOUR_INDEX
+    RA_ERROR,
+    DECLINATION_ERROR,
+    PARALLAX_ERR,
+    COLOUR_INDEX,
+    COLOUR_INDEX_ERROR,
+    VI_COLOUR_INDEX
+};
+
+const char field_descriptions[][40] = {
+    "Hipparcos identifier",
+    "Solution type new reduction",
+    "Solution type old reduction",
+    "Number of components",
+    "Right Ascension in ICRS, Ep=1991.25",
+    "Declination in ICRS, Ep=1991.25",
+    "Parallax",
+    "Proper motion in Right Ascension",
+    "Proper motion in Declination",
+    "Formal error on RArad",
+    "Formal error on DErad",
+    "Formal error on Plx",
+    "Colour index",
+    "Formal error on colour index",
+    "V-I colour index"
 };
 
 }
